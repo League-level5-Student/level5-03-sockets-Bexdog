e045d794b18e5a4e2e63f2a2c5132d0ca6956c20
beckh <beckh@McKid>
2020-09-27 11:24:31 -0700

    Chat Client and server

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -11,7 +11,7 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      String ip = "192.168.1.211";
+      String ip = "192.168.1.227";
       //2. Create an integer for the server's port number
       int pn = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
--- src/_02_Chat_Application/ChatServer.java
+++ src/_02_Chat_Application/ChatServer.java
@@ -1,7 +1,87 @@
 package _02_Chat_Application;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+import javax.swing.JOptionPane;
+
+import _01_Intro_To_Sockets.server.ServerGreeter;
+
 public class ChatServer {
+	ServerSocket ss;
 	public ChatServer() {
 		
-	}
+			//2. Initialize the ServerSocket object. In the parameters,
+			//   you must define the port at which the server will listen for connections.
+			try {
+				ss = new ServerSocket(8080);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//*OPTIONAL* you can set a time limit for the server to wait by using the 
+			//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
+		}
+
+		public void run() {
+			//3. Create a boolean variable and initialize it to true.
+			boolean b = true;
+			//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
+				while(b) {
+				//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
+				//   Put steps 8 - 15 in the try block.
+					try {
+					//8. Let the user know that the server is waiting for a client to connect.
+						System.out.println("*robotic voice*\nWaiting for client to connect");
+					//9. Create an object of the Socket class and initialize it to serverSocket.accept();
+					//   Change serverSocket to match the ServerSocket member variable you created in step 1.
+					//   The program will wait here until either a client connects or the timeout expires.
+						Socket sc= ss.accept();
+					//10. Let the user know that the client has connected.
+						System.out.println("The client has connected");
+					//11. Create a DataInputStream object. When initializing it, use the Socket object you created in step 9 to call the getInputStream() method.
+						DataInputStream dis = new DataInputStream(sc.getInputStream());
+					//12. Print the message from the DataInputStream object using the readUTF() method
+						JOptionPane.showMessageDialog(null, "Client: "+dis.readUTF());
+					//13. Create a DataOutputStream object. When initializing it, use the Server object you created in step 9 to call the getOutputStream() method.
+						DataOutputStream dos = new DataOutputStream(sc.getOutputStream());
+					//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
+						String output = JOptionPane.showInputDialog("Message:");
+					dos.writeUTF(output);
+					//15. Close the client server
+					ss.close();
+					}
+					catch(SocketTimeoutException e) {
+						System.out.println("Error Error Error Error\nsocket timeout exception");
+						b = false;
+					}
+					catch(IOException e) {
+						System.out.println("Error Error Error Error\nIO Exception");
+						b = false;
+					}
+					
+				}
+				//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
+
+				//7. If the program catches a IOException, let the user know about it and set the loop's boolean variable to false.
+
+			
+		}
+
+		public static void main(String[] args) {
+			//16. In a new thread, create an object of the ServerGreeter class and start the thread. Don't forget the try-catch.
+			Thread i = new Thread(()-> {
+				try {
+					ServerGreeter sg = new ServerGreeter();
+					sg.run();
+				} catch(IOException e1){
+					e1.printStackTrace();
+				}
+			});
+			i.start();
+		}
 	}

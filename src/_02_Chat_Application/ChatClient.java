a68fdae4758776fcc19d47373ae5bd8e7d60d2fd
beckh <beckh@McKid>
2020-10-04 10:08:46 -0700

    Testing

--- src/_02_Chat_Application/ChatClient.java
+++ src/_02_Chat_Application/ChatClient.java
@@ -4,30 +4,32 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.JOptionPane;
 
 public class ChatClient {
-	public static void main(String [] args) {
-		  //1. Create a String for the ip address of the server. 
-		  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-	      String ip = "192.168.1.227";
+	public static void main(String [] args) throws UnknownHostException, IOException {
+		  String ip = "192.168.1.240";
 	      //2. Create an integer for the server's port number
 	      int pn = 8080;
 	      //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
 	    boolean i = true;
+	    Socket s;
+		
+	    DataOutputStream DOS;
+	    DataInputStream DIS;
+		
+			s = new Socket(ip,pn);
+			DOS = new DataOutputStream(s.getOutputStream());
+			DIS = new DataInputStream(s.getInputStream());
+		
+	   
 	    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
 	    while(i) {
 	      try {
-			Socket s = new Socket(ip,pn);
-		
-	         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-	         DataOutputStream DOS = new DataOutputStream(s.getOutputStream());
-	         //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-	        String output = JOptionPane.showInputDialog("Message:");
+			String output = JOptionPane.showInputDialog("Message:");
 	        DOS.writeUTF(output);
-	         //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-	        DataInputStream DIS = new DataInputStream(s.getInputStream());
 	         //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
 	         JOptionPane.showMessageDialog(null, "Server: "+DIS.readUTF());
 	         //9. Close the client's server object
--- src/_02_Chat_Application/ChatServer.java
+++ src/_02_Chat_Application/ChatServer.java
@@ -27,33 +27,20 @@ public class ChatServer {
 			//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
 		}
 
-		public void run() {
+		public void run() throws IOException {
 			//3. Create a boolean variable and initialize it to true.
 			boolean b = true;
 			//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
+			System.out.println("*robotic voice*\nWaiting for client to connect");
+			Socket sc= ss.accept();
+			DataInputStream dis = new DataInputStream(sc.getInputStream());
+			DataOutputStream dos = new DataOutputStream(sc.getOutputStream());
 				while(b) {
-				//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
-				//   Put steps 8 - 15 in the try block.
 					try {
-					//8. Let the user know that the server is waiting for a client to connect.
-						System.out.println("*robotic voice*\nWaiting for client to connect");
-					//9. Create an object of the Socket class and initialize it to serverSocket.accept();
-					//   Change serverSocket to match the ServerSocket member variable you created in step 1.
-					//   The program will wait here until either a client connects or the timeout expires.
-						Socket sc= ss.accept();
-					//10. Let the user know that the client has connected.
 						System.out.println("The client has connected");
-					//11. Create a DataInputStream object. When initializing it, use the Socket object you created in step 9 to call the getInputStream() method.
-						DataInputStream dis = new DataInputStream(sc.getInputStream());
-					//12. Print the message from the DataInputStream object using the readUTF() method
-						JOptionPane.showMessageDialog(null, "Client: "+dis.readUTF());
-					//13. Create a DataOutputStream object. When initializing it, use the Server object you created in step 9 to call the getOutputStream() method.
-						DataOutputStream dos = new DataOutputStream(sc.getOutputStream());
-					//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
+						JOptionPane.showMessageDialog(null, "Client: "+dis.readUTF());	
 						String output = JOptionPane.showInputDialog("Message:");
 					dos.writeUTF(output);
-					//15. Close the client server
-					ss.close();
 					}
 					catch(SocketTimeoutException e) {
 						System.out.println("Error Error Error Error\nsocket timeout exception");
@@ -65,11 +52,6 @@ public class ChatServer {
 					}
 					
 				}
-				//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
-
-				//7. If the program catches a IOException, let the user know about it and set the loop's boolean variable to false.
-
-			
 		}
 
 		public static void main(String[] args) {
